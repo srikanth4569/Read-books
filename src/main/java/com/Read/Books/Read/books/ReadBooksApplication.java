a4d3a3998f54d2154a7b380e240d984406709f32
srikanth4569 <89223644+srikanth4569@users.noreply.github.com>
2023-05-12 15:26:16 -0500

    Initial commit

---
+++ src/main/java/com/Read/Books/Read/books/ReadBooksApplication.java
@@ -0,0 +1,13 @@
+package com.Read.Books.Read.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadBooksApplication.class, args);
+	}
+
+}
